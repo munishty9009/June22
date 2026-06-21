05e95c77d0b40f06d1794aa7f7eda8255d3c1b76
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2026-06-21 10:51:49 -0400

    Update output.tf

--- Modules/Subnet&PIP/output.tf
+++ Modules/Subnet&PIP/output.tf
@@ -1,10 +1,8 @@
 
-output "subnet_id"    
-{ 
+output "subnet_id"    { 
    value = azurerm_subnet.subnet.id 
 }
 
-output "public_ip_id" 
-{ 
+output "public_ip_id" { 
    value = azurerm_public_ip.public_ip.id 
 }
