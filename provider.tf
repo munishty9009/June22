16f620ad8f2c06d0dd00662dabc4cfaa131e68cd
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2026-06-21 06:30:21 -0400

    Add Terraform provider configuration for Azure

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.5.0"
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+    virtual_machine {
+      delete_os_disk_on_deletion     = true
+      graceful_shutdown              = false
+      skip_shutdown_and_force_delete = false
+    }
+    key_vault {
+      purge_soft_delete_on_destroy    = false
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+
+}
