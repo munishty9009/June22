5868bbd508761069e3826ff2360e10091dbf77d6
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2026-06-21 06:47:51 -0400

    Refactor variable declarations in variables.tf
    Refactor variable declarations for better readability.

--- Modules/VNET/variables.tf
+++ Modules/VNET/variables.tf
@@ -1,5 +1,20 @@
 
-variable "resource_group_name" { type = string }
-variable "location"            { type = string }
-variable "vnet_name"           { type = string }
-variable "address_space"       { type = list(string) }
+variable "resource_group_name" 
+{ 
+type = string 
+}
+
+variable "location"            
+{ 
+type = string 
+}
+
+variable "vnet_name"           
+{ 
+type = string 
+}
+
+variable "address_space"       
+{ 
+type = list(string) 
+}
