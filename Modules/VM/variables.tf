b55af45ce91f6b300cd59d1150f4ce40437cf6a8
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2026-06-21 11:14:00 -0400

    Update variables.tf

--- Modules/VM/variables.tf
+++ Modules/VM/variables.tf
@@ -8,9 +8,23 @@ variable "vm_size"             {
   type = string
   default = "Standard_B2s" 
   }
-variable "admin_username"      { type = string }
+variable "admin_username"      { 
+  type = string 
+}
 variable "ssh_public_key"      { type = string }
-variable "image_publisher"     { type = string; default = "Canonical" }
-variable "image_offer"         { type = string; default = "0001-com-ubuntu-server-jammy" }
-variable "image_sku"           { type = string; default = "22_04-lts" }
-variable "image_version"       { type = string; default = "latest" }
+variable "image_publisher"     {
+  type = string
+  default = "Canonical" 
+}
+variable "image_offer"         {
+  type = string 
+  default = "0001-com-ubuntu-server-jammy" 
+}
+variable "image_sku"           {
+  type = string  
+  default = "22_04-lts" 
+}
+variable "image_version"       {
+  type = string 
+  default = "latest" 
+}
