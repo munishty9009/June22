23f4225686a9fa3eb6677fe51d1c830e4884c977
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2026-06-21 13:26:59 -0400

    Change output to reference Windows VM ID

--- Modules/VM/output.tf
+++ Modules/VM/output.tf
@@ -1,3 +1,3 @@
 
-output "vm_id"      { value = azurerm_linux_virtual_machine.vm.id }
+output "vm_id"      { value = azurerm_windows_virtual_machine.vm.id }
 output "private_ip" { value = azurerm_network_interface.nic.private_ip_address }
