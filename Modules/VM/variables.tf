3ccb050dbe5b8cc02d50f24147f36bc3acf53c39
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2026-06-21 06:55:17 -0400

    Add variables for VM configuration in Terraform

--- Modules/VM/variables.tf
+++ Modules/VM/variables.tf
@@ -1,1 +1,13 @@
 
+variable "vm_name"             { type = string }
+variable "resource_group_name" { type = string }
+variable "location"            { type = string }
+variable "subnet_id"           { type = string }
+variable "public_ip_id"        { type = string }
+variable "vm_size"             { type = string; default = "Standard_B2s" }
+variable "admin_username"      { type = string }
+variable "ssh_public_key"      { type = string }
+variable "image_publisher"     { type = string; default = "Canonical" }
+variable "image_offer"         { type = string; default = "0001-com-ubuntu-server-jammy" }
+variable "image_sku"           { type = string; default = "22_04-lts" }
+variable "image_version"       { type = string; default = "latest" }
