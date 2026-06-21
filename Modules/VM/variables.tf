141f2b906b066f4a887911ba4da609029e5a3f8e
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2026-06-21 11:11:27 -0400

    Update variables.tf

--- Modules/VM/variables.tf
+++ Modules/VM/variables.tf
@@ -4,7 +4,10 @@ variable "resource_group_name" { type = string }
 variable "location"            { type = string }
 variable "subnet_id"           { type = string }
 variable "public_ip_id"        { type = string }
-variable "vm_size"             { type = string; default = "Standard_B2s" }
+variable "vm_size"             { 
+  type = string
+  default = "Standard_B2s" 
+  }
 variable "admin_username"      { type = string }
 variable "ssh_public_key"      { type = string }
 variable "image_publisher"     { type = string; default = "Canonical" }
