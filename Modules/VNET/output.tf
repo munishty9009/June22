6e12afcfc447d488e644a6964d68d56df7213e3f
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2026-06-21 06:46:55 -0400

    Add outputs for VNet ID, resource group name, and location

--- Modules/VNET/output.tf
+++ Modules/VNET/output.tf
@@ -1,1 +1,15 @@
 
+output "vnet_id"             
+{ 
+value = azurerm_virtual_network.vnet.id 
+}
+
+output "resource_group_name" 
+{ 
+value = azurerm_resource_group.rg.name
+}
+
+output "location"         
+{ 
+value = azurerm_resource_group.rg.location 
+}
