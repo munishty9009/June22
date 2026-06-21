3a2d54cab3926df0d352d010bc6db11bf58a0c59
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2026-06-21 06:46:02 -0400

    Add variables for resource group and VNET configuration

--- Modules/VNET/variables.tf
+++ Modules/VNET/variables.tf
@@ -1,1 +1,5 @@
 
+variable "resource_group_name" { type = string }
+variable "location"            { type = string }
+variable "vnet_name"           { type = string }
+variable "address_space"       { type = list(string) }
