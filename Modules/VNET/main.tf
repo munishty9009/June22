df705387b061208b76e1d71ce5f8ff468a27a9d3
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2026-06-21 12:52:22 -0400

    Add dependency on resource group for VNET

--- Modules/VNET/main.tf
+++ Modules/VNET/main.tf
@@ -9,4 +9,5 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = var.address_space
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  depends_on = [azurerm_resource_group.rg] 
 }
