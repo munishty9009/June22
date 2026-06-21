742500f5c0160221284bfbbb3e718e87bbfa1d7d
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2026-06-21 13:21:12 -0400

    Update main.tf

--- Modules/VM/main.tf
+++ Modules/VM/main.tf
@@ -1,4 +1,3 @@
-
 resource "azurerm_network_interface" "nic" {
   name                = "${var.vm_name}-nic"
   location            = var.location
@@ -12,26 +11,24 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-resource "azurerm_widnows_virtual_machine" "vm" {
-  name                = var.vm_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  size                = var.vm_size
-  admin_username      = var.admin_username
-
+resource "azurerm_windows_virtual_machine" "vm" {    # ✅ fixed typo
+  name                  = var.vm_name
+  resource_group_name   = var.resource_group_name
+  location              = var.location
+  size                  = var.vm_size
+  admin_username        = var.admin_username
+  admin_password        = var.admin_password
   network_interface_ids = [azurerm_network_interface.nic.id]
- 
-  }
 
-  os_disk {
+  os_disk {                                           # ✅ now inside the block
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
 
-  source_image_reference {
+  source_image_reference {                            # ✅ now inside the block
     publisher = var.image_publisher
     offer     = var.image_offer
     sku       = var.image_sku
     version   = var.image_version
   }
-
+}                                                     # ✅ brace closes here
