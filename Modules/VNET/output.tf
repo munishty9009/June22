563859ccd4caac2270a2717090aec30777bf0c37
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2026-06-21 10:44:25 -0400

    Fix output formatting in output.tf

--- Modules/VNET/output.tf
+++ Modules/VNET/output.tf
@@ -1,15 +1,15 @@
 
 output "vnet_id"             
 { 
-value = azurerm_virtual_network.vnet.id 
+   value = azurerm_virtual_network.vnet.id 
 }
 
 output "resource_group_name" 
 { 
-value = azurerm_resource_group.rg.name
+   value = azurerm_resource_group.rg.name
 }
 
 output "location"         
 { 
-value = azurerm_resource_group.rg.location 
+   value = azurerm_resource_group.rg.location 
 }
